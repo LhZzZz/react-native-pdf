c55cfecf88c757dccc8cc189e6157c6099d6c506
LhZzZz <lhz644133940@sina.com>
2020-06-06 18:17:42 +0800

    切换成通过Uri获取InputStream后生成pdf到指定路径后再转换成图片

--- android/src/main/java/com/reactlibrary/PdfModule.java
+++ android/src/main/java/com/reactlibrary/PdfModule.java
@@ -30,12 +30,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 
 public class PdfModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    public String pdfSaveDir = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "snapread_pdf";
+    public String pdfImgSaveDir = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "snapread_pdfImg";
 
     public PdfModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -53,14 +57,13 @@ public class PdfModule extends ReactContextBaseJavaModule {
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
     }
 
+    //获取真实路径(处理不完全,弃用)
     private String getFilePathFromContentUri(Uri uri) {
         String filePath;
         if (isExternalStorageDocument(uri)){
             final String docId = DocumentsContract.getDocumentId(uri);
-            System.out.println("------docID "+ docId);
             final String[] split = docId.split(":");
             final String type = split[0];
-            System.out.println("getPath() docId:" + docId +", split:" + split.length +", type:" + type);
             // This is for checking Main Memory
             if ("primary".equalsIgnoreCase(type)) {
                 if (split.length > 1) {
@@ -74,9 +77,13 @@ public class PdfModule extends ReactContextBaseJavaModule {
             }
         } else if (isDownloadsDocument(uri)){
             final String id = DocumentsContract.getDocumentId(uri);
-            final Uri contentUri = ContentUris.withAppendedId(
-                    Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-
+            Uri contentUri = uri;
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
+                contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+            }
+//            final Uri contentUri = ContentUris.withAppendedId(
+//                    Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
             String[] projection = { MediaStore.Images.Media.DATA };
             Cursor cursor = reactContext.getContentResolver().query(contentUri, projection, null, null, null);
             int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
@@ -90,15 +97,43 @@ public class PdfModule extends ReactContextBaseJavaModule {
             filePath = cursor.getString(columnIndex);
             cursor.close();
         }
-        System.out.println("------path: " + filePath);
         return filePath;
     }
 
+    //根据uri生成pdf文件
+    private String createdPdf(Uri uri, Callback callback){
+        File saveDir = new File(pdfSaveDir);
+        String pdfFileName = "snapread.pdf";
+        String pdfPath = null;
+        try {
+            InputStream pdfStream =  reactContext.getContentResolver().openInputStream(uri);
+            if (!saveDir.exists()){//文件夹不存在就新建一个
+               saveDir.mkdirs();
+            }
+            pdfPath = saveDir + File.separator + pdfFileName;
+            File targetFile = new File(pdfPath);
+            OutputStream os = new FileOutputStream(targetFile);
+            int ch = -1;
+            byte[] buf = new byte[1024];
+            while ((ch = pdfStream.read(buf)) != -1){
+                os.write(buf,0,ch);
+            }
+            os.flush();
+            os.close();
+        }catch (IOException e){
+            callback.invoke("error");
+        }
+        return pdfPath;
+    }
+
     @ReactMethod
-    public void convertPdf(String pdfPath, boolean isUri, Callback callback) throws IOException {
+    public void convertPdf(String pdfPath, Callback callback){
         ArrayList<Bitmap> bitmaps = new ArrayList<>();
-        if (isUri){
-            pdfPath = getFilePathFromContentUri(Uri.parse(pdfPath));
+        Uri pdfUri = Uri.parse(pdfPath);
+        if (pdfUri.getScheme().equalsIgnoreCase("content")){
+            pdfPath = createdPdf(pdfUri,callback);
+        }else {
+            pdfPath = pdfUri.getPath();
         }
         if (pdfPath!=null){
             File pdfFile = new File(pdfPath);
@@ -140,12 +175,13 @@ public class PdfModule extends ReactContextBaseJavaModule {
         }
     }
 
+    //存为图片
     private JSONArray saveImg(ArrayList<Bitmap> bitmaps, Callback callback){
-        File saveDir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "snapread_pdf");
+        File saveDir = new File(pdfImgSaveDir);
         JSONArray Imgarray = new JSONArray();
         for (int i = 0; i<bitmaps.size();i++){
             if (!saveDir.exists()){//文件夹不存在就新建一个
-                saveDir.mkdir();
+                saveDir.mkdirs();
             }
             String pdfImgName = "snapread_" + i + ".jpg";//保存的图片的名称
             File pdfImgFile = new File(saveDir,pdfImgName);
@@ -166,13 +202,10 @@ public class PdfModule extends ReactContextBaseJavaModule {
                 Imgarray.put(i,imgInfo);
             } catch (FileNotFoundException e) {
                 callback.invoke("error");
-//                e.printStackTrace();
             } catch (IOException e) {
                 callback.invoke("error");
-//                e.printStackTrace();
             } catch (JSONException e) {
                 callback.invoke("error");
-//                e.printStackTrace();
             }
 
         }
